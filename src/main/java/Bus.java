001f7aa3597971abff213c54f49e2dec3c647872
Victor <victorsetlhare@gmail.com>
2020-06-16 15:03:59 +0100

    Bus destination

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -1,13 +1,19 @@
 public class Bus {
 
     private int routeNumber;
+    private String destination;
 
-    public Bus(int routeNumber){
+    public Bus(int routeNumber, String destination){
         this.routeNumber = routeNumber;
+        this.destination = destination;
     }
 
     public int getRouteNumber(){
         return this.routeNumber;
+    }
+
+    public String getDestination(){
+        return this.destination;
     }
 
 }
