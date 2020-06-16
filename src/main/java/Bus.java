833e2692033f1942cbce3af400204d1042281302
Victor <victorsetlhare@gmail.com>
2020-06-16 14:55:28 +0100

    Bus test route

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -1,2 +1,12 @@
 public class Bus {
+
+    private int routeNumber;
+
+    public Bus(int routeNumber){
+        this.routeNumber = routeNumber;
+    }
+
+    public int getRouteNumber(){
+        return this.routeNumber;
+    }
 }
--- src/test/java/BusTest.java
+++ src/test/java/BusTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class BusTest {
+    private Bus bus;
+
+
+    @Before
+    public void before(){
+    bus = new Bus(22);
+   }
+
+    @Test
+     public void hasRouteNumber(){
+        assertEquals(22, bus.getRouteNumber());
+        }
 }
+
+
